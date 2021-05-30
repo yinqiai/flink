3dbe0962907055a5ac27642b4642696c9ec7317e
yinqi <qi.yin@transsnet.com>
2021-05-30 09:49:55 +0800

    2021053001

--- src/main/scala/com.transsnet.study/windowsFunction/CountWindows.scala
+++ src/main/scala/com.transsnet.study/windowsFunction/CountWindows.scala
@@ -4,12 +4,13 @@ import com.transsnet.study.userSource.UserSource.SensorReading
 import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
 import org.apache.flink.streaming.api.windowing.time.Time
 import org.apache.flink.api.scala._
-
+import org.apache.flink.streaming.api.datastream.DataStream
 /**
   * @author yinqi
-  * @date 2021/5/27
+  * @date 2021/5/28
   */
-object TimeWindows {
+object CountWindows {
+
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
 
@@ -18,23 +19,25 @@ object TimeWindows {
     //要测试窗口，需要流数据，因为直接用本地文本数据，很快就读取完了，窗口还没输出程序就关闭了
     //val txt = env.readTextFile("C:\\Users\\11597\\Desktop\\palmpay_new\\flink\\src\\main\\resources\\table_test.txt")
     val txt = env.socketTextStream("hadoop000", 9000)
-    val reduceDataStream =txt.map( data => { val dataArray = data.split(",")
+    val dataStream =txt.map( data => { val dataArray = data.split(",")
       SensorReading(dataArray(0).trim, dataArray(1).trim.toLong, dataArray(2).trim.toDouble) })
-      //.keyBy("id") .reduce( (x, y) => SensorReading(x.id, x.timestamp + 1, y.temperature) )
+    //.keyBy("id") .reduce( (x, y) => SensorReading(x.id, x.timestamp + 1, y.temperature) )
     //1.滚动窗口
     /*val minTempPerWindow = reduceDataStream.map(r => (r.id, r.temperature,r.timestamp))
       .keyBy(_._1)
       .timeWindow(Time.seconds(15))
       .reduce((r1, r2) => (r1._1, r1._2.min(r2._2),r2._3))*/
 
-    //2.滑动窗口
-    val minTempPerWindow = reduceDataStream.map(r => (r.id, r.temperature,r.timestamp))
+    //2.count窗口
+    val countWindow = dataStream .map(r => (r.id, r.temperature))
       .keyBy(_._1)
-      .timeWindow(Time.seconds(15),Time.seconds(5))
-      .reduce((r1, r2) => (r1._1, r1._2.min(r2._2),r2._3))
-    minTempPerWindow.print()
+      .countWindow(5)
+      .reduce((r1, r2) => (r1._1, r1._2.max(r2._2)))
 
+    countWindow.print()
     env.execute()
   }
+
+
 
 }
